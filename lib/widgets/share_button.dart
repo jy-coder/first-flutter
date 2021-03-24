9965bc732f02d79dd1f767c61d66109cdce4b8b2
JTD3 <joshuatdthree@yahoo.com>
2021-03-25 00:04:24 +0800

    Added sharing and bookmark indicator

---
+++ lib/widgets/share_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+import 'package:newheadline/provider/theme.dart';
+import 'package:provider/provider.dart';
+
+class ShareBtn extends StatefulWidget {
+  final String link;
+
+  ShareBtn({this.link});
+
+  @override
+  _ShareBtnState createState() => _ShareBtnState();
+}
+
+class _ShareBtnState extends State<ShareBtn> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        icon: Icon(Icons.share, size: 15),
+        onPressed: () {
+          share(context, this.widget.link);
+        });
+  }
+}
+
+void share(BuildContext context, String link) {
+  Share.share(link, subject: link);
+}
