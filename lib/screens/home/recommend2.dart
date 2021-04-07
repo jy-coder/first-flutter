d8828686e94cae8be33b91a3102e6a08c6ac167d
laurencjy <cjyclx@gmail.com>
2021-04-07 11:10:03 +0800

    update: testing advertisement

--- lib/screens/home/recommend2.dart
+++ lib/screens/home/recommend2.dart
@@ -18,6 +18,7 @@ class _RecommendScreen2State extends State<RecommendScreen2>
   TabController _tabController;
   List<String> categoryNames = [];
   bool _isLoading = true;
+  AdMobService ad;
 
   @override
   void initState() {
@@ -49,6 +50,7 @@ class _RecommendScreen2State extends State<RecommendScreen2>
 
   @override
   Widget build(BuildContext context) {
+    AdMobService.createPublishAd();
     ArticleProvider hProvider =
         Provider.of<ArticleProvider>(context, listen: true);
     articles = hProvider.items;
@@ -56,27 +58,24 @@ class _RecommendScreen2State extends State<RecommendScreen2>
     return _isLoading
         ? CircularProgressIndicator(backgroundColor: Colors.grey)
         // : !_isLoading
-        //     ? 
-        //     ListView.builder(
-        //         padding: const EdgeInsets.all(10.0),
-        //         itemCount: articles.length,
-        //         itemBuilder: (ctx, i) {
-        //           return ArticleCard(
-        //             articles[i].articleId,
-        //             articles[i].title,
-        //             articles[i].imageUrl,
-        //             articles[i].summary,
-        //             articles[i].link,
-        //             articles[i].description,
-        //             articles[i].pubDate,
-        //             articles[i].source,
-        //             articles[i].category,
-        //           );
-        //         })
-            // : Container();
-    :ElevatedButton(
-        child: Text("click here"),
-        onPressed: () => AdMobService.createBannerAd(),
-      );
+        //   ?
+        // ListView.builder(
+        //     padding: const EdgeInsets.all(10.0),
+        //     itemCount: articles.length,
+        //     itemBuilder: (ctx, i) {
+        //       return ArticleCard(
+        //         articles[i].articleId,
+        //         articles[i].title,
+        //         articles[i].imageUrl,
+        //         articles[i].summary,
+        //         articles[i].link,
+        //         articles[i].description,
+        //         articles[i].pubDate,
+        //         articles[i].source,
+        //         articles[i].category,
+        //       );
+        //     })
+        // : Container();
+        : Container(child: AdMobService.publisherAd, height: 300, width: 300);
   }
 }
--- lib/utils/admob.dart
+++ lib/utils/admob.dart
@@ -3,10 +3,12 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AdMobService {
+  static AdWidget adWidget = AdWidget(ad: banner);
+  static AdWidget publisherAd = AdWidget(ad: myBanner);
+
   String getAdMobAppId() {
     if (Platform.isAndroid) {
       print(env['ADMOB_APP_ID']);
-      
     }
     return null;
   }
@@ -19,6 +21,7 @@ class AdMobService {
   }
 
   static BannerAd banner;
+  static PublisherBannerAd myBanner;
 
   static void createBannerAd() {
     banner = BannerAd(
@@ -44,6 +47,39 @@ class AdMobService {
         // Called when an ad is in the process of leaving the application.
         onApplicationExit: (Ad ad) => print('Left application.'),
       ),
+    )..load();
+  }
+
+  static PublisherAdRequest request = PublisherAdRequest(
+    keywords: <String>['flutterio', 'beautiful apps'],
+    contentUrl: 'https://flutter.dev',
+    customTargetingLists: <String, List<String>>{
+      'favoriteColors': <String>['red', 'yellow']
+    },
+  );
+
+  static AdListener listener = AdListener(
+    // Called when an ad is successfully received.
+    onAdLoaded: (Ad ad) => print('Ad loaded.'),
+    // Called when an ad request failed.
+    onAdFailedToLoad: (Ad ad, LoadAdError error) {
+      ad.dispose();
+      print('Ad failed to load: $error');
+    },
+    // Called when an ad opens an overlay that covers the screen.
+    onAdOpened: (Ad ad) => print('Ad opened.'),
+    // Called when an ad removes an overlay that covers the screen.
+    onAdClosed: (Ad ad) => print('Ad closed.'),
+    // Called when an ad is in the process of leaving the application.
+    onApplicationExit: (Ad ad) => print('Left application.'),
+  );
+
+  static void createPublishAd() {
+    myBanner = PublisherBannerAd(
+      adUnitId: _getBannerAdId(),
+      sizes: [AdSize.banner],
+      request: request,
+      listener: listener,
     )..load();
   }
 }
