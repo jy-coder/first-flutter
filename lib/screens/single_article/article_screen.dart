339cade2b7ee5b9d8506f750327de27202a937be
JTD3 <80634269+JTD3@users.noreply.github.com>
2021-04-10 17:05:23 +0800

    Update: Save reading history upon scrolling to bottom of page (#20)

--- lib/screens/single_article/article_screen.dart
+++ lib/screens/single_article/article_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:newheadline/provider/article.dart';
 import 'package:newheadline/provider/theme.dart';
 import 'package:newheadline/screens/single_article/webview_screen.dart';
 import 'package:newheadline/shared/textstyle.dart';
 import 'package:newheadline/utils/common.dart';
+import 'package:newheadline/utils/response.dart';
+import 'package:newheadline/utils/urls.dart';
 import 'package:provider/provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
@@ -30,108 +33,126 @@ class ArticleScreen extends StatefulWidget {
   _ArticleScreenState createState() => _ArticleScreenState();
 }
 
+Future<void> saveReadingHistory(int articleId) async {
+  String url = "$HISTORY_URL/?article=$articleId";
+  var result = await APIService().post(url);
+}
+
 class _ArticleScreenState extends State<ArticleScreen> {
   @override
   Widget build(BuildContext context) {
     String timeAgo = formatDate(widget.pubDate);
     ThemeProvider tProvider =
         Provider.of<ThemeProvider>(context, listen: false);
-
-    return SingleChildScrollView(
-      child: Container(
-        child: Wrap(
-          children: [
-            Container(
-                height: 300,
-                width: 500,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: CachedNetworkImage(
-                    imageUrl: widget.imageUrl,
-                    progressIndicatorBuilder:
-                        (context, url, downloadProgress) => SizedBox(
-                      child: CircularProgressIndicator(
-                          value: downloadProgress.progress),
-                      height: 30,
-                      width: 30,
-                    ),
-                    errorWidget: (context, ud, error) => Icon(Icons.error),
-                  ),
-                )),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.only(top: 5),
-              padding: EdgeInsets.all(5),
-              child: Column(
-                children: [
-                  Text(
-                    widget.title,
-                    style: CustomTextStyle.title1(context, tProvider.fontSize),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
+    ArticleProvider aProvider =
+        Provider.of<ArticleProvider>(context, listen: false);
+    return NotificationListener<ScrollNotification>(
+        onNotification: (scrollNotification) {
+          if (scrollNotification is ScrollEndNotification &&
+              scrollNotification.metrics.pixels ==
+                  scrollNotification.metrics.maxScrollExtent &&
+              aProvider.tab != "Setting") {
+            saveReadingHistory(widget.id);
+          }
+        },
+        child: SingleChildScrollView(
+          child: Container(
+            child: Wrap(
+              children: [
+                Container(
+                    height: 300,
+                    width: 500,
+                    child: Align(
                       alignment: Alignment.center,
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                      child: CachedNetworkImage(
+                        imageUrl: widget.imageUrl,
+                        progressIndicatorBuilder:
+                            (context, url, downloadProgress) => SizedBox(
+                          child: CircularProgressIndicator(
+                              value: downloadProgress.progress),
+                          height: 30,
+                          width: 30,
+                        ),
+                        errorWidget: (context, ud, error) => Icon(Icons.error),
+                      ),
+                    )),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(top: 5),
+                  padding: EdgeInsets.all(5),
+                  child: Column(
+                    children: [
+                      Text(
+                        widget.title,
+                        style:
+                            CustomTextStyle.title1(context, tProvider.fontSize),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                          alignment: Alignment.center,
+                          child: Column(
                             children: [
-                              Text(
-                                timeAgo,
-                                style: CustomTextStyle.small(
-                                    context, tProvider.fontSize),
-                                textAlign: TextAlign.center,
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.timelapse),
+                                  Text(
+                                    timeAgo,
+                                    style: CustomTextStyle.small(
+                                        context, tProvider.fontSize),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
                               ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.source),
+                                  Text(
+                                    widget.source,
+                                    style: CustomTextStyle.small(
+                                        context, tProvider.fontSize),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              )
                             ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                widget.source,
-                                style: CustomTextStyle.small(
-                                    context, tProvider.fontSize),
-                                textAlign: TextAlign.center,
-                              ),
-                            ],
-                          )
-                        ],
-                      )),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      widget.description,
-                      style:
-                          CustomTextStyle.normal(context, tProvider.fontSize),
-                    ),
-                  ),
-                  Container(
-                    child: OutlineButton(
-                      padding: EdgeInsets.all(20),
-                      child: Text("Visit Website",
+                          )),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          widget.description,
                           style: CustomTextStyle.normal(
-                              context, tProvider.fontSize)),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    WebViewScreen(this.widget.link)));
-                      },
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+                              context, tProvider.fontSize),
+                        ),
+                      ),
+                      Container(
+                        child: OutlineButton(
+                          padding: EdgeInsets.all(20),
+                          child: Text("Visit Website",
+                              style: CustomTextStyle.normal(
+                                  context, tProvider.fontSize)),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        WebViewScreen(this.widget.link)));
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
