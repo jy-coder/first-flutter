da97ce9e01fb850ad55e29850bf87a0f0a5ff4c8
JTD3 <joshuatdthree@yahoo.com>
2021-04-22 19:00:18 +0800

    Update: Scroll to top button

--- lib/screens/single_article/article_screen.dart
+++ lib/screens/single_article/article_screen.dart
@@ -43,6 +43,7 @@ Future<void> saveReadingHistory(int articleId) async {
 
 class _ArticleScreenState extends State<ArticleScreen> {
   List<Article> relatedArticles = [];
+  ScrollController _scrollController;
 
   @override
   void didChangeDependencies() {
@@ -55,6 +56,31 @@ class _ArticleScreenState extends State<ArticleScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController()
+      ..addListener(() {
+        setState(() {
+          if (_scrollController.offset >=
+              _scrollController.position.maxScrollExtent) {
+            saveReadingHistory(widget.id);
+          }
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(0,
+        duration: Duration(milliseconds: 300), curve: Curves.linear);
+  }
+
+  @override
   Widget build(BuildContext context) {
     String timeAgo = formatDate(widget.pubDate);
     ThemeProvider tProvider =
@@ -62,161 +88,174 @@ class _ArticleScreenState extends State<ArticleScreen> {
     ArticleProvider aProvider =
         Provider.of<ArticleProvider>(context, listen: false);
 
-    return NotificationListener<ScrollNotification>(
-        onNotification: (scrollNotification) {
-          if (scrollNotification is ScrollEndNotification &&
-              scrollNotification.metrics.pixels ==
-                  scrollNotification.metrics.maxScrollExtent &&
-              aProvider.tab != "Setting") {
-            saveReadingHistory(aProvider.articleId);
-          }
-        },
-        child: SingleChildScrollView(
-          child: Container(
-            child: Wrap(
-              children: [
-                Container(
-                    height: 300,
-                    width: 500,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: CachedNetworkImage(
-                        imageUrl: widget.imageUrl,
-                        progressIndicatorBuilder:
-                            (context, url, downloadProgress) => SizedBox(
-                          child: CircularProgressIndicator(
-                              value: downloadProgress.progress),
-                          height: 30,
-                          width: 30,
-                        ),
-                        errorWidget: (context, ud, error) => Icon(Icons.error),
-                      ),
-                    )),
-                Container(
+    return SingleChildScrollView(
+      controller: _scrollController,
+      child: Container(
+        child: Wrap(
+          children: [
+            Container(
+                height: 300,
+                width: 500,
+                child: Align(
                   alignment: Alignment.center,
-                  margin: EdgeInsets.only(top: 5),
-                  padding: EdgeInsets.all(5),
-                  child: Column(
-                    children: [
-                      Text(
-                        widget.title,
-                        style:
-                            CustomTextStyle.title1(context, tProvider.fontSize),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                          alignment: Alignment.center,
-                          child: Column(
+                  child: CachedNetworkImage(
+                    imageUrl: widget.imageUrl,
+                    progressIndicatorBuilder:
+                        (context, url, downloadProgress) => SizedBox(
+                      child: CircularProgressIndicator(
+                          value: downloadProgress.progress),
+                      height: 30,
+                      width: 30,
+                    ),
+                    errorWidget: (context, ud, error) => Icon(Icons.error),
+                  ),
+                )),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 5),
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: [
+                  Text(
+                    widget.title,
+                    style: CustomTextStyle.title1(context, tProvider.fontSize),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                      alignment: Alignment.center,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    timeAgo,
-                                    style: CustomTextStyle.small(
-                                        context, tProvider.fontSize),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ],
+                              Text(
+                                timeAgo,
+                                style: CustomTextStyle.small(
+                                    context, tProvider.fontSize),
+                                textAlign: TextAlign.center,
                               ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    widget.source,
-                                    style: CustomTextStyle.small(
-                                        context, tProvider.fontSize),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ],
-                              )
                             ],
-                          )),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          widget.description,
-                          style: CustomTextStyle.normal(
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                widget.source,
+                                style: CustomTextStyle.small(
+                                    context, tProvider.fontSize),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          )
+                        ],
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      widget.description,
+                      style:
+                          CustomTextStyle.normal(context, tProvider.fontSize),
+                    ),
+                  ),
+                  aProvider.relatedArticle.length > 0
+                      ? Text(
+                          "More Articles:",
+                          style: CustomTextStyle.title1(
                               context, tProvider.fontSize),
-                        ),
-                      ),
-                      aProvider.relatedArticle.length > 0
-                          ? Text(
-                              "More Articles:",
-                              style: CustomTextStyle.title1(
-                                  context, tProvider.fontSize),
-                            )
-                          : Container(),
-                      SizedBox(height: 20),
-                      Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            ...aProvider.relatedArticle
-                                .map(
-                                  (Article a) => GestureDetector(
-                                      child: Column(
-                                        children: [
-                                          Text(
-                                            a.title,
-                                            textAlign: TextAlign.start,
-                                            style:
-                                                CustomTextStyle.underlinelink(
-                                                    context,
-                                                    tProvider.fontSize),
-                                          ),
-                                          SizedBox(height: 10)
-                                        ],
+                        )
+                      : Container(),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        ...aProvider.relatedArticle
+                            .map(
+                              (Article a) => GestureDetector(
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        a.title,
+                                        textAlign: TextAlign.start,
+                                        style: CustomTextStyle.underlinelink(
+                                            context, tProvider.fontSize),
                                       ),
-                                      onTap: () {
-                                        Navigator.of(context).pushNamed(
-                                          "RelatedScreen",
-                                          arguments: ScreenArguments(
-                                            a.articleId,
-                                            a.title,
-                                            a.description,
-                                            a.imageUrl,
-                                            a.pubDate,
-                                            a.source,
-                                            a.category,
-                                            a.link,
-                                          ),
-                                        );
-                                      }),
-                                )
-                                .toList(),
-                            SizedBox(height: 20),
-                          ],
-                        ),
+                                      SizedBox(height: 10)
+                                    ],
+                                  ),
+                                  onTap: () {
+                                    Navigator.of(context).pushNamed(
+                                      "RelatedScreen",
+                                      arguments: ScreenArguments(
+                                        a.articleId,
+                                        a.title,
+                                        a.description,
+                                        a.imageUrl,
+                                        a.pubDate,
+                                        a.source,
+                                        a.category,
+                                        a.link,
+                                      ),
+                                    );
+                                  }),
+                            )
+                            .toList(),
+                        SizedBox(height: 20),
+                      ],
+                    ),
+                  ),
+                  Container(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(width: 130),
+                      OutlineButton(
+                        padding: EdgeInsets.all(20),
+                        child: Text("Visit Website",
+                            style: CustomTextStyle.normal(
+                                context, tProvider.fontSize)),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      WebViewScreen(this.widget.link)));
+                          _scrollToTop();
+                        },
                       ),
-                      Container(
-                        child: OutlineButton(
-                          padding: EdgeInsets.all(20),
-                          child: Text("Visit Website",
-                              style: CustomTextStyle.normal(
-                                  context, tProvider.fontSize)),
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        WebViewScreen(this.widget.link)));
-                          },
+                      SizedBox(width: 30),
+                      MaterialButton(
+                        onPressed: () {
+                          _scrollToTop();
+                        },
+                        color: tProvider.theme == "dark"
+                            ? Colors.white
+                            : Colors.black,
+                        textColor: tProvider.theme == "dark"
+                            ? Colors.black
+                            : Colors.white,
+                        child: Icon(
+                          Icons.arrow_upward,
+                          size: 24,
                         ),
+                        padding: EdgeInsets.all(16),
+                        shape: CircleBorder(),
                       )
                     ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ));
+                  ))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
